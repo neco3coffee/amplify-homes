aedbb29054f651c75c515e1fc097c7034752c75b
neco3coffee <neco3coffee@gmail.com>
2021-12-06 01:01:07 +0900

    vercelにホスティングするためにaws-exports.jsを.gitignoreから削除

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import Amplify from 'aws-amplify';
-import "@aws-amplify/ui-react/styles.css";
-import {AmplifyProvider} from "@aws-amplify/ui-react";
-import awsconfig from './aws-exports';
-Amplify.configure(awsconfig);
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+import Amplify from 'aws-amplify'
+import '@aws-amplify/ui-react/styles.css'
+import { AmplifyProvider } from '@aws-amplify/ui-react'
+import awsconfig from './aws-exports'
+Amplify.configure(awsconfig)
 
 ReactDOM.render(
   <React.StrictMode>
@@ -16,9 +16,9 @@ ReactDOM.render(
     </AmplifyProvider>
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
